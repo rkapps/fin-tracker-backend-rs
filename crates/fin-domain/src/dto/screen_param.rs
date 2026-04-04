9b8cc50ea1f34d969a105d6da42187ab6af9e295
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-04-03 19:48:08 -0700

    add bin cli

--- crates/fin-domain/src/dto/screen_param.rs
+++ crates/fin-domain/src/dto/screen_param.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::ticker::Ticker;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TickerScreenParam {
     pub query: Option<String>, // semantic: "cloud security", "payments infrastructure"
@@ -9,4 +11,20 @@ pub struct TickerScreenParam {
     pub asset_type: Option<String>, // "stock", "etf"
     pub limit: Option<usize>,
     pub r#yield: Option<f32>,
+}
+
+impl TickerScreenParam {
+
+    pub fn new_for_asset_type(asset_type: &str) -> TickerScreenParam {
+        TickerScreenParam{
+            asset_type: Some(asset_type.to_string()),
+            industry: None,
+            limit: None,
+            market_cap_range: None,
+            query: None,
+            signals: None,
+            r#yield: None
+        }
+
+    }
 }
