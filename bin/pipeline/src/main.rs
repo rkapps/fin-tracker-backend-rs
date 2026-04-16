0a01e7ef3e826e5dde932b35e27827d8494a2bb1
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-04-15 21:05:40 -0700

    add bin pipeline

---
+++ bin/pipeline/src/main.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use bin_shared::{
+    logger::set_logger,
+    services::{get_ml_service, get_pipeline_service},
+};
+use clap::{Parser, Subcommand};
+use tracing::{error, info};
+
+#[derive(Parser)]
+#[command(name = "pipeline")]
+struct Cli {
+    #[command(subcommand)]
+    command: PipelineCommands,
+}
+
+#[derive(Subcommand)]
+enum PipelineCommands {
+    TickersEod,
+    TickersRealtimeStocksEtfs,
+    TickersRealtimeCrypto,
+    TrainModel,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    set_logger();
+
+    let cli = Cli::parse();
+    let pipeline_service = get_pipeline_service().await?;
+
+    match cli.command {
+        PipelineCommands::TickersEod => {
+            info!("Tickers EOD PipeLine started...");
+            match pipeline_service.update_tickers_eod().await {
+                Ok(_) => info!("Tickers EOD update completed successfully."),
+                Err(e) => error!("Tickers EOD update failed: {:?}", e),
+            }
+
+            match pipeline_service
+                .update_ticker_eod_prediction_signals()
+                .await
+            {
+                Ok(_) => info!("Tickers EOD prediction signals completed successfully."),
+                Err(e) => error!("Tickers EOD prediction signals failed: {:?}", e),
+            }
+            info!("Tickers EOD PipeLine done.");
+        }
+        PipelineCommands::TickersRealtimeStocksEtfs => {
+            info!("Tickers Stocks and Etfs Realtime started...");
+            match pipeline_service.update_tickers_realtime_stocks_etfs().await {
+                Ok(_) => info!("Tickers Stocks and Etfs Realtime completed successfully."),
+                Err(e) => error!("Tickers Stocks and Etfs Realtime failed: {:?}", e),
+            }
+        }
+        PipelineCommands::TickersRealtimeCrypto => {
+            info!("Tickers Crypto Realtime started...");
+            match pipeline_service.update_tickers_realtime_stocks_etfs().await {
+                Ok(_) => info!("Tickers Crypto Realtime completed successfully."),
+                Err(e) => error!("Tickers Crypto Realtime failed: {:?}", e),
+            }
+        }
+        PipelineCommands::TrainModel => {
+            info!("Tickers Training Model started...");
+            let ml_service = get_ml_service().await?;
+            match ml_service.build_and_train_all().await {
+                Ok(_) => info!("Tickers Training Model completed successfully."),
+                Err(e) => error!("Tickers Training Model failed: {:?}", e),
+            }
+        }
+    }
+
+    Ok(())
+}
